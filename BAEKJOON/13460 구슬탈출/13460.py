ca2bd77a9eee0873cb7779179f81a240e01f9ca9
jngmk <rlawjd7186@naver.com>
2019-10-18 17:59:27 +0900

    backup

--- BAEKJOON/13460 구슬탈출/13460.py
+++ BAEKJOON/13460 구슬탈출/13460.py
@@ -3,7 +3,6 @@ from collections import deque
 
 def straight(aa, bb, dd):
     while True:
-        # print(aa, bb)
         if arr[aa+da[dd]][bb+db[dd]] == '.':
             aa = aa+da[dd]
             bb = bb+db[dd]
@@ -14,152 +13,73 @@ def straight(aa, bb, dd):
     return aa, bb
 
 
-# def bfs():
-#     global result
-#     q.append((r1, r2, b1, b2, 1))
-#     while True:
-#         # print(q)
-#         if result != -1:
-#             break
-#         if not q:
-#             break
-#         rr1, rr2, bb1, bb2, cnt = q.popleft()
-#         # print('-----------------')
-#         # print('pop', rr1, rr2, bb1, bb2)
-#         for d in range(4):
-#             if d == 0:
-#                 if rr1 >= bb1:
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                 else:
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#             elif d == 1:
-#                 if bb2 >= rr2:
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                 else:
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#             elif d == 2:
-#                 if bb1 >= rr1:
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                 else:
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#             else:
-#                 if rr2 >= bb2:
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                 else:
-#                     vra, vrb = straight(rr1, rr2, d)
-#                     arr[vra][vrb] = 'R'
-#                     vba, vbb = straight(bb1, bb2, d)
-#                     arr[vba][vbb] = 'B'
-#             # print(arr)
-#             # print(vra, vrb, vba, vbb, d)
-#             if vba == vbb == -1:
-#                 break
-#             elif vra == vrb == -1:
-#                 result = cnt
-#                 break
-#             if not r_visited[(vra, vrb)] or not b_visited[(vba, vbb)]:
-#                 q.append((vra, vrb, vba, vbb, cnt+1))
-#                 r_visited[(vra, vrb)] = 1
-#                 b_visited[(vba, vbb)] = 1
-#             arr[vra][vrb] = '.'
-#             arr[vba][vbb] = '.'
-
-
-def dfs(rr1, rr2, bb1, bb2, cnt):
-    global result, arr
-    # print(q)
-    if result <= cnt:
-        return
-    if bb1 == bb2 == -1:
-        result = -1
-        return
-    if rr1 == rr2 == -1:
-        result = cnt
-        return
-    print('-----------------')
-    print('pop', rr1, rr2, bb1, bb2)
-    for d in range(4):
-        if d == 0:
-            if rr1 >= bb1:
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-            else:
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-        elif d == 1:
-            if bb2 >= rr2:
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-            else:
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-        elif d == 2:
-            if bb1 >= rr1:
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-            else:
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-        else:
-            if rr2 >= bb2:
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-            else:
-                vra, vrb = straight(rr1, rr2, d)
-                arr[vra][vrb] = 'R'
-                vba, vbb = straight(bb1, bb2, d)
-                arr[vba][vbb] = 'B'
-        print(arr)
-        print(vra, vrb, vba, vbb, d)
-        if vba == vbb == -1:
-            result = -1
+def bfs():
+    global result
+    q.append((r1, r2, b1, b2, 1))
+    while True:
+        if not q:
+            break
+        rr1, rr2, bb1, bb2, cnt = q.popleft()
+        # print('pop', rr1, rr2, bb1, bb2)
+        if cnt == 11:
             return
-        if vra == vrb == -1:
-            result = cnt
-            return
-        if not r_visited[(vra, vrb)] or not b_visited[(vba, vbb)]:
-            r_visited[(vra, vrb)] = 1
-            b_visited[(vba, vbb)] = 1
+        for d in range(4):
+            if d == 0:
+                if rr1 >= bb1:
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                else:
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+            elif d == 1:
+                if bb2 >= rr2:
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                else:
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+            elif d == 2:
+                if bb1 >= rr1:
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                else:
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+            else:
+                if rr2 >= bb2:
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                else:
+                    vra, vrb = straight(rr1, rr2, d)
+                    arr[vra][vrb] = 'R'
+                    vba, vbb = straight(bb1, bb2, d)
+                    arr[vba][vbb] = 'B'
+            if vba == vbb == -1:
+                arr[vra][vrb] = '.'
+                arr[vba][vbb] = '.'
+                continue
+            if vra == vrb == -1:
+                result = cnt
+                return
+            if not visited.get((vra, vrb, vba, vbb)):
+                q.append((vra, vrb, vba, vbb, cnt+1))
+                visited.update({(vra, vrb, vba, vbb): 1})
             arr[vra][vrb] = '.'
             arr[vba][vbb] = '.'
-            q.append((vra, vrb, vba, vbb, cnt+1))
-            r_visited[(vra, vrb)] = 0
-            b_visited[(vba, vbb)] = 0
 
 
 da = [-1, 0, 1, 0]
@@ -167,34 +87,17 @@ db = [0, 1, 0, -1]
 N, M = map(int, input().split())
 arr = [list(input()) for _ in range(N)]
 q = deque()
-tmp_cnt = 0
-# result = -1
-result = 9999999
-r_visited = {(i, j): 0 for i in range(1, N-1) for j in range(1, M-1)}
-b_visited = {(i, j): 0 for i in range(1, N-1) for j in range(1, M-1)}
-# print(r_visited)
+result = -1
+visited = {}
 # 시작 위치 저장
 for a in range(1, N-1):
     for b in range(1, M-1):
-        if tmp_cnt == 3:
-            break
         if arr[a][b] == 'R':
             r1, r2 = a, b
             arr[a][b] = '.'
-            r_visited.update({(a, b): 1})
-            tmp_cnt += 1
         elif arr[a][b] == 'B':
             arr[a][b] = '.'
             b1, b2 = a, b
-            b_visited.update({(a, b): 1})
-            tmp_cnt += 1
-        elif arr[a][b] == 'O':
-            o1, o2 = a, b
-            tmp_cnt += 1
-    if tmp_cnt == 3:
-        break
-# print(arr)
-# bfs()
-dfs(r1, r2, b1, b2, 1)
-# 탐색
+visited.update({(r1, r2, b1, b2): 1})
+bfs()
 print(result)
--- BAEKJOON/16235 나무재태크/16235.py
+++ BAEKJOON/16235 나무재태크/16235.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 N, M, K = map(int, input().split())
 S2D2 = [list(map(int, input().split())) for _ in range(N)]
 soil = [[5] * N for _ in range(N)]
@@ -6,6 +8,7 @@ dead_tree = []
 breeding = [[[] for _ in range(N)] for _ in range(N)]
 da = [-1, -1, -1, 0, 0, 1, 1, 1]
 db = [-1, 0, 1, -1, 1, -1, 0, 1]
+result = M
 
 for _ in range(M):
     a, b, age = map(int, input().split())
@@ -13,8 +16,9 @@ for _ in range(M):
 
 for _ in range(K):
     # spring
-    print(trees)
-    print(soil)
+    # pprint(trees)
+    # pprint(soil)
+    # print('---------------------------')
     for a in range(N):
         for b in range(N):
             tmp = len(trees[a][b])
@@ -23,17 +27,20 @@ for _ in range(K):
                 if soil[a][b] >= trees[a][b][age]:
                     soil[a][b] -= trees[a][b][age]
                     trees[a][b][age] += 1
-                    if trees[a][b][age] == 5:
+                    if trees[a][b][age] % 5 == 0:
                         for d in range(8):
                             if 0 <= a + da[d] < N and 0 <= b + db[d] < N:
                                 breeding[a+da[d]][b+db[d]].append(1)
+                                result += 1
                 else:
                     idx = age
+                    # print(idx)
                     break
             if idx >= 0:
                 for age in range(idx, -1, -1):
                     soil[a][b] += trees[a][b][age]//2
                     trees[a][b].pop(age)
+                    result -= 1
             soil[a][b] += S2D2[a][b]
 
     for a in range(N):
@@ -41,10 +48,5 @@ for _ in range(K):
             if breeding[a][b]:
                 trees[a][b].extend(breeding[a][b])
                 breeding[a][b] = []
-
-result = 0
-for a in range(N):
-    for b in range(N):
-        result += len(trees[a][b])
 
 print(result)
